8ba51d1126fb941027e9fe20a3a04da6468ccd34
mlaursen <mlaursen03@gmail.com>
2015-03-02 12:07:46 -0500

    Boop boop mcscoop calendar stuffs

--- src/main/webapp/resources/js/controllers/CalendarCtrl.js
+++ src/main/webapp/resources/js/controllers/CalendarCtrl.js
@@ -4,4 +4,24 @@ angular.module('MyBeerApp')
     'use strict';
 
     $scope.title = "Loading...";
+    
+    $scope.currentDate = new Date();
+    $scope.currentYear = $scope.currentDate.getYear() + 1900;
+    $scope.currentMonth = $scope.currentDate.getMonth();
+
+    $scope.previousMonth = function() {
+      console.log('Previous Month!');
+    }
+
+    $scope.nextMonth = function() {
+      console.log('Next month!');
+    }
+
+    $scope.setMonth = function(month) {
+      $scope.currentDate.setMonth(month);
+    }
+
+    $scope.setYear = function(year) {
+      $scope.currentDate.setYear(year - 1900);
+    }
   });

f642825456c7eea1cfa40c83320221e0700ebd0d
mlaursen <mlaursen03@gmail.com>
2015-03-05 11:21:08 -0500

    Updated schema

--- src/main/scripts/tables.sql
+++ src/main/scripts/tables.sql
@@ -1,3 +1,5 @@
+-------------------------------------------------
+-- Lookup Tables
 CREATE TABLE Yeast
 ( id   SERIAL
 , name TEXT
@@ -8,69 +10,123 @@ CREATE TABLE Yeast
 CREATE TABLE Malt
 ( id     SERIAL
 , name   TEXT
-, amount DECIMAL
-, unit   TEXT
 , CONSTRAINT pk_Malt_Id PRIMARY KEY(id)
 );
 
 CREATE TABLE Grain
 ( id     SERIAL
 , name   TEXT
-, amount DECIMAL
-, unit   TEXT
 , CONSTRAINT pk_Grain_Id PRIMARY KEY(id)
 );
 
 CREATE TABLE Hops
 ( id       SERIAL
 , name     TEXT
-, amount   DECIMAL
-, unit     TEXT
-, boilTime INTEGER
 , CONSTRAINT pk_Hops_Id PRIMARY KEY(id)
 );
 
-CREATE TABLE Recipe_Part
-( id       SERIAL
-, malt_id  INTEGER
-, grain_id INTEGER
-, hops_id  INTEGER
-, CONSTRAINT pk_Recipe_Part_Id       PRIMARY KEY(id)
-, CONSTRAINT fk_Recipe_Part_Malt_Id  FOREIGN KEY(malt_id)
-                                     REFERENCES Malt(id)
-, CONSTRAINT fk_Recipe_Part_Grain_Id FOREIGN KEY(grain_id)
-                                     REFERENCES Grain(id)
-, CONSTRAINT fk_Recipe_Part_Hops_Id  FOREIGN KEY(hops_id)
-                                     REFERENCES Hops(id)
+CREATE TABLE Event_Type
+( id    SERIAL
+, name  TEXT
+, color TEXT
+, CONSTRAINT pk_Event_Type_Id PRIMARY KEY(id)
+);
+
+CREATE TABLE Beer_Color
+( name TEXT
+, CONSTRAINT pk_Beer_Color_Name PRIMARY KEY(name)
+);
+
+CREATE TABLE Beer_Style
+( name TEXT
+, CONSTRAINT pk_Beer_Style_Name PRIMARY KEY(name)
+);
+
+---------------------------------------------------
+-- Table Data
+CREATE TABLE Beer
+( id               SERIAL
+, name             TEXT
+, style            TEXT
+, color            TEXT
+, description      TEXT
+, CONSTRAINT pk_Beer_Id PRIMARY KEY(id)
+, CONSTRAINT fk_Beer_Beer_Style
+    FOREIGN KEY(style) REFERENCES Beer_Style(name)
+, CONSTRAINT fk_Beer_Beer_Color
+    FOREIGN KEY(color) REFERENCES Beer_Color(name)
 );
 
 CREATE TABLE Recipe
 ( id             SERIAL
-, recipe_part_id INTEGER
 , yeast_id       INTEGER
-, CONSTRAINT pk_Recipe_Id             PRIMARY KEY(id)
-, CONSTRAINT fk_Recipe_Recipe_Part_Id FOREIGN KEY(recipe_part_id)
-                                      REFERENCES Recipe_Part(id)
-, constraint fk_Recipe_Yeast_Id       FOREIGN KEY (yeast_id)
-                                      REFERENCES Yeast(id)
+, boil_time      INTEGER
+, CONSTRAINT pk_Recipe_Id PRIMARY KEY(id)
+, CONSTRAINT fk_Recipe_Yeast_Id 
+      FOREIGN KEY (yeast_id) REFERENCES Yeast(id)
 );
 
-
-CREATE TABLE Beer
-( id               SERIAL
-, recipe_id        INTEGER
-, name             TEXT
-, original_gravity DECIMAL
-, final_gravity    DECIMAL
-, abv              DECIMAL
-, date_brewed      DATE
-, primary_end      DATE
-, secondary_end    DATE
-, drink_date       DATE
-, style            TEXT
-, color            TEXT
-, description      TEXT
-, CONSTRAINT pk_Beer_Id        PRIMARY KEY(id)
-, CONSTRAINT fk_Beer_Recipe_Id FOREIGN KEY(recipe_id)
-                               REFERENCES Recipe(id)
+CREATE TABLE Brewed_Beer
+( id          SERIAL
+, beer_id     INTEGER
+, recipe_id   INTEGER
+, og          DECIMAL
+, fg          DECIMAL
+, ibu         INTEGER -- international bittering units
+, abv         DECIMAL -- alcohol by volume
+, description TEXT
+, CONSTRAINT pk_Brewed_Beer_Id PRIMARY KEY(id)
+, CONSTRAINT fk_Brewed_Beer_Beer_Id
+    FOREIGN KEY(beer_id) REFERENCES Beer(id)
+, CONSTRAINT fk_Brewed_Beer_Recipe_Id
+    FOREIGN KEY(recipe_id) REFERENCES Recipe(id)
 );
+
+CREATE TABLE Event
+( brewed_beer_id INTEGER
+, event_type_id  INTEGER
+, event_date     DATE
+, CONSTRAINT pk_Event PRIMARY KEY(brewed_beer_id, event_type_id)
+, CONSTRAINT fk_Event_Brewed_Beer_Id
+    FOREIGN KEY(brewed_beer_id) REFERENCES Brewed_Beer(id)
+, CONSTRAINT fk_Event_Event_Type_Id
+    FOREIGN KEY(event_type_id) REFERENCES Event_Type(id)
+);
+
+CREATE TABLE Recipe_Malt
+( recipe_id INTEGER
+, malt_id   INTEGER
+, amount    DECIMAL
+, unit      TEXT
+, CONSTRAINT pk_Recipe_Malt PRIMARY KEY(recipe_id, malt_id)
+, CONSTRAINT fk_Recipe_Malt_Recipe_Id
+    FOREIGN KEY(recipe_id) REFERENCES Recipe(id)
+, CONSTRAINT fk_Recipe_Malt_Malt_Id
+    FOREIGN KEY(malt_id) REFERENCES Malt(id)
+);
+
+CREATE TABLE Recipe_Grain
+( recipe_id INTEGER
+, grain_id  INTEGER
+, amount    DECIMAL
+, unit      TEXT
+, CONSTRAINT pk_Recipe_Grain PRIMARY KEY(recipe_id, grain_id)
+, CONSTRAINT fk_Recipe_Hops_Recipe_Id
+    FOREIGN KEY(recipe_id) REFERENCES Recipe(id)
+, CONSTRAINT fk_Recipe_Hops_Grain_Id
+    FOREIGN KEY(grain_id) REFERENCES Grain(id)
+);
+
+CREATE TABLE Recipe_Hops
+( recipe_id INTEGER
+, hops_id   INTEGER
+, amount    DECIMAL
+, unit      TEXT
+, boil_time INTEGER
+, CONSTRAINT pk_Recipe_Hops PRIMARY KEY(recipe_id, hops_id)
+, CONSTRAINT fk_Recipe_Hops_Recipe_Id
+    FOREIGN KEY(recipe_id) REFERENCES Recipe(id)
+, CONSTRAINT fk_Recipe_Hops_Hops_Id
+    FOREIGN KEY(hops_id) REFERENCES Hops(id)
+);
+

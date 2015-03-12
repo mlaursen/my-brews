cccd2935bfe95ea1543d3109435d3df53e201e1e
mlaursen <mlaursen03@gmail.com>
2015-03-12 19:22:24 -0400

    Updated compass to work with moved frontend stuff

--- config.rb
+++ config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "src/main/webapp/resources/css"
+css_dir = "src/main/frontend/resources/css"
 sass_dir = "src/main/scripts/sass"
-images_dir = "src/main/webapp/resources/imgs"
-javascripts_dir = "src/main/webapp/resources/js"
+images_dir = "src/main/frontend/resources/imgs"
+javascripts_dir = "src/main/frontend/resources/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed

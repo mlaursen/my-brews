71b002e3de064c4b086dc283501677b1d56ce20b
mlaursen <mlaursen03@gmail.com>
2015-03-18 19:40:25 -0400

    Removed unused stuff. and moving from dialogs for the beer info

--- src/main/frontend/resources/js/app.js
+++ src/main/frontend/resources/js/app.js
@@ -6,24 +6,16 @@ angular.module('MyBeerApp', [
 ).config(function ($routeProvider) {
   'use strict';
 
-  $routeProvider
-    .when('/calendar', {
+  $routeProvider.when('/calendar', {
       templateUrl: 'views/calendar.html',
       controller: 'CalendarCtrl'
-    })
-    .when('/calendar/:date', {
-      templateUrl: 'views/calendarDate.html',
-      controller: 'CalendarDateCtrl'
-    })
-    .when('/beer', {
+    }).when('/beer', {
       templateUrl: 'views/beers.html',
       controller: 'BeerCtrl'
-    })
-    .when('/beer/:id', {
+    }).when('/beer/:id', {
       templateUrl: 'views/beer.html',
       controller: 'BeerCtrl'
-    })
-    .otherwise({
+    }).otherwise({
       redirectTo: '/beer'
     });
 

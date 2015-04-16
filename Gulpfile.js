19eefabddc0f92ce2a2e757dd8f5b95db27178e4
mlaursen <mlaursen03@gmail.com>
2015-04-15 20:54:47 -0400

    Fixed build to make dist and only add disted libs

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,114 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
-    path = require('path');
+    path = require('path'),
+    merge = require('merge-stream');
+
+var srcDir = 'src/main/frontend';
+var jsDir = srcDir + '/js';
+var sassDir = srcDir + '/scss';
+
+var libDir = 'bower_components';
+
+var distDir = 'dist';
+var distCssDir = distDir + '/css';
+var distJsDir  = distDir + '/js';
+var distLibDir = distDir + '/lib';
+var distImgDir = distDir + '/imgs';
+
+// Constant Configurations
+var SASS_CONFIG = {
+  style: 'expanded',
+};
+var BROWSERIFY_CONFIG = {
+  insertGlobals: true,
+  debug: !gulp.env.production
+};
+var MIN = {
+  suffix: '.min'
+};
+
+// Compile sass files, auto-prefix elements,  copy to app's css directory
+// and creates minified version as well
+gulp.task('styles', function() {
+  return sass(sassDir, SASS_CONFIG)
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest(distCssDir))
+    .pipe(rename(MIN))
+    .pipe(minifycss())
+    .pipe(gulp.dest(distCssDir));
+});
+
+
+// Compiles the react files into a single app.js and copie it to the distribution folder
+gulp.task('scripts', function() {
+  return gulp.src(jsDir + '/app.js')
+    .pipe(browserify(BROWSERIFY_CONFIG))
+    .pipe(gulp.dest(distJsDir));
+});
+
+
+// Copies all the libs to the distribution folder
+gulp.task('libs', function() {
+  var bootstrap = gulp.src(libDir + '/bootstrap/dist/**/*')
+    .pipe(gulp.dest(distLibDir + '/bootstrap'));
+
+  var fontAwesomeCss = gulp.src(libDir + '/font-awesome/**/*.css')
+    .pipe(gulp.dest(distLibDir + '/font-awesome'));
+
+  var fontAwesomeFonts = gulp.src(libDir + '/font-awesome/fonts/*')
+    .pipe(gulp.dest(distLibDir + '/font-awesome/fonts'));
+
+  var jquery = gulp.src(libDir + '/jquery/dist/*.js')
+    .pipe(gulp.dest(distLibDir + '/jquery'));
+
+  var normalize = gulp.src(libDir + '/normalize-css/normalize.css')
+    .pipe(gulp.dest(distLibDir + '/normalize-css'));
+
+  return merge(bootstrap, fontAwesomeCss, fontAwesomeFonts, jquery, normalize);
+});
+
+
+// Copies all the html pages to the dist folder
+gulp.task('pages', function() {
+  return gulp.src(srcDir + '/**/*.html')
+    .pipe(gulp.dest(distDir));
+});
+
+
+gulp.task('imgs', function() {
+  return gulp.src(srcDir + '/imgs/**/*')
+    .pipe(gulp.dest(distImgDir));
+});
+
+
+// builds the distribution package by copying all the libs, css, javascript files, and html pages to the dist folder.
+gulp.task('dist', ['libs', 'styles', 'scripts', 'pages', 'imgs']);
+
+
+// Watches files to auto compile and copy
+gulp.task('watch', function() {
+  // Watching for compiling
+  gulp.watch(sassDir + '/**/*.scss', ['styles']);
+  gulp.watch(jsDir + '/**/*.js', ['scripts']);
+  gulp.watch(srcDir + '/**/*.html', ['pages']);
+
+  // Watching to live reload
+  gulp.watch(distDir + '/**/*.html', liveReload);
+  gulp.watch(distCssDir + '/**/*.css', liveReload);
+  gulp.watch(distJsDir + '/**/*.js', liveReload);
+});
+
+
+
+// Starts the development server on port 4000
+gulp.task('express', function() {
+  var express = require('express');
+  var app     = express();
+  app.use(require('connect-livereload')({port: 4002}));
+  app.use(express.static(path.join(__dirname, distDir)));
+  app.listen(4000);
+});
 
 var tinylr;
 gulp.task('livereload', function() {
@@ -23,71 +130,10 @@ function liveReload(event) {
   });
 }
 
-var srcDir = 'src/main/frontend';
-var sassDir = srcDir + '/scss';
-var cssDir = srcDir + '/css';
-var jsDir = srcDir + '/js';
-var compiledJSX = 'build/js';
-
-var SASS_CONFIG = {
-  style: 'expanded',
-  //includePaths: [srcDir + '/lib']
-};
-
-// Compile sass files, auto-prefix elements,  copy to app's css directory
-// and creates minified version as well
-gulp.task('css', function() {
-  return sass(sassDir, SASS_CONFIG)
-    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-    .pipe(gulp.dest(cssDir))
-    .pipe(rename({ suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest(cssDir));
-});
-
-
-// Watches files to auto compile and copy
-gulp.task('watch', function() {
-  gulp.watch(sassDir + '/**/*.scss', ['css']);
-  gulp.watch(srcDir + '/**/*.html', liveReload);
-  gulp.watch(cssDir + '/**/*.css', liveReload);
-  gulp.watch([jsDir + '/**/*.js', '!' + jsDir + '/bundle.js'], ['bundle']);
-  gulp.watch(jsDir + '/bundle.js', liveReload);
-});
-
-
-// Uses browserify to convert/compile jsx files using the app.js single source.
-// Outputs the built javascript file into build/
-gulp.task('build', function() {
-return gulp.src(jsDir + '/app.js')
-      .pipe(browserify({
-        insertGlobals: true,
-        debug: !gulp.env.production
-      }))
-      .pipe(gulp.dest(compiledJSX));
-});
-
-// Takes the built javascript file and moves it to bundle.js in the active directory.. Prolly
-// a better way to do this.
-gulp.task('bundle', ['build'], function() {
-  return gulp.src(compiledJSX + '/app.js')
-      .pipe(rename('bundle.js'))
-      .pipe(gulp.dest(jsDir));
-});
-
-
-// Starts the development server on port 4000
-gulp.task('express', function() {
-  var express = require('express');
-  var app     = express();
-  app.use(require('connect-livereload')({port: 4002}));
-  app.use(express.static(path.join(__dirname, 'src/main/frontend')));
-  app.listen(4000);
-});
 
 
 
 
-gulp.task('default', ['css', 'bundle', 'express', 'livereload', 'watch'], function() {
+gulp.task('default', ['dist', 'express', 'livereload', 'watch'], function() {
 
 });
